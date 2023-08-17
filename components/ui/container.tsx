bbf7aa4fa1e460355cb513989fc2061a1284064a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-17 09:42:12 +0900

    🎉 feat(ui): add Container component
    The Container component is a wrapper component that provides a responsive container for its children. It accepts an optional className prop for additional styling. The component uses the tailwind-merge library to merge the base class name with the provided className prop.

---
+++ components/ui/container.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge";
+
+type Props = React.HTMLAttributes<HTMLDivElement>;
+
+export const Container = ({ children, className, ...props }: Props) => {
+	const BASE_CLASS_NAME =
+		"container mx-auto px-4 sm:max-w-[90%] md:max-w-[85%] xl:max-w-[70%]";
+
+	return (
+		<div {...props} className={twMerge([BASE_CLASS_NAME, className])}>
+			{children}
+		</div>
+	);
+};
