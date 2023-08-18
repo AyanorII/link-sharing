1526495daf079b1f91169eeb9689f66ff713f44c
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-18 15:03:11 +0900

    add "Form" components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./container";
+export * from "./form";
 export * from "./input";
 export * from "./label";
 export * from "./select";
