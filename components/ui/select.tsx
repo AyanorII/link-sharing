a941c3feac7f12132f9b46a56b7a868da8c514c4
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-18 16:01:00 +0900

    fix Select menu clicking on behind positioned element

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -37,10 +37,14 @@ SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
 const SelectContent = React.forwardRef<
 	React.ElementRef<typeof SelectPrimitive.Content>,
 	React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, position = "popper", ...props }) => (
 	<SelectPrimitive.Portal>
 		<SelectPrimitive.Content
-			ref={ref}
+			// Reference: https://github.com/radix-ui/primitives/issues/1658#issuecomment-1664079551
+			ref={(ref) => {
+				if (!ref) return;
+				ref.ontouchstart = (e) => e.preventDefault();
+			}}
 			className={cn(
 				"relative z-50 min-w-[8rem] top-2 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-2xl data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
 				position === "popper" &&
