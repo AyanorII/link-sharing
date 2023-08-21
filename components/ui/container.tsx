846e2b53ddca937473ad3d3ac8a6a90e360f0a28
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-21 16:33:28 +0900

    remove xl container breakpoint

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -4,7 +4,7 @@ type Props = React.HTMLAttributes<HTMLDivElement>;
 
 export const Container = ({ children, className, ...props }: Props) => {
 	const BASE_CLASS_NAME =
-		"container mx-auto px-4 sm:max-w-[90%] md:max-w-[85%] xl:max-w-[70%]";
+		"container mx-auto px-4 sm:max-w-[90%] md:max-w-[85%]";
 
 	return (
 		<div {...props} className={twMerge([BASE_CLASS_NAME, className])}>
