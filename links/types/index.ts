f33b33751583cb28f6d851c360d5f7c6e021830a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-21 12:19:45 +0900

    add LinkWithPlatform interface

--- links/types/index.ts
+++ links/types/index.ts
@@ -1,6 +1,11 @@
 import { Database } from "@/lib/database.types";
+import { Platform } from "@/platforms/types";
 
 export type Link = Database["public"]["Tables"]["links"]["Row"];
 export type InsertLink = Database["public"]["Tables"]["links"]["Insert"];
 export type UpdateLink = Database["public"]["Tables"]["links"]["Update"];
 export type UpsertLink = InsertLink | UpdateLink;
+
+export interface LinkWithPlatform extends Link {
+	platform: Platform;
+}
