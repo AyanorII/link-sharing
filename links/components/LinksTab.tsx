6bd3ab47a5b6178f3734ae6286dcebfb381e3a3d
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-08-23 10:33:20 +0900

    show only logged user links on form

--- links/components/LinksTab.tsx
+++ links/components/LinksTab.tsx
@@ -1,27 +1,30 @@
 import { getPlatforms } from "@/platforms/actions";
+import { getProfile } from "@/profiles/actions";
 
 import { getSupabaseServerClient } from "@/lib/supabase/utils";
 
-import { getLinks } from "../actions";
 import { LinkForm } from "./LinkForm";
 
 export const LinksTab = async () => {
 	const supabase = getSupabaseServerClient("component");
 
-	const [{ data: platforms }, { data: links }] = await Promise.all([
-		getPlatforms(),
-		getLinks(),
-	]);
+	const { data: platforms } = await getPlatforms();
 
 	const {
 		data: { user },
 	} = await supabase.auth.getUser();
+
+	const profile = await getProfile(user!.id);
 
 	if (!user) {
 		return null;
 	}
 
 	return (
-		<LinkForm user={user} platforms={platforms || []} links={links || []} />
+		<LinkForm
+			user={user}
+			platforms={platforms || []}
+			links={profile.links || []}
+		/>
 	);
 };
